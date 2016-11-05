5a180a58e075f07db9ebbfb5108bb11ec05c56ce
amit itzkovitch <amit@localhost.localdomain>
2016-11-05 11:11:39 -0700

    first version of panda-service works

---
+++ roles/panda-service/files/panda-service.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python2.7
+
+import BaseHTTPServer
+import SimpleHTTPServer
+import os
+
+RESOURCES_DIR_PATH = './resources'
+SERVER_ADDRESS = ('0.0.0.0', 8888)
+
+def start_http_server(dir_path, server_port):
+    os.chdir(dir_path)
+    http_server = BaseHTTPServer.HTTPServer(SERVER_ADDRESS, 
+                                            SimpleHTTPServer.SimpleHTTPRequestHandler)
+    http_server.serve_forever()
+
+
+def main():
+    start_http_server(RESOURCES_DIR_PATH, 8080)
+
+if __name__ == '__main__':
+    main()
+
