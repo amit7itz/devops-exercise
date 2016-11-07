19d04519f2dd445cbf270448de7e76ffa2e510f2
amit <amit7itz@gmail.com>
2016-11-07 12:21:40 -0800

    orgenized the script properly :)

--- roles/big-service/files/big-service.py
+++ roles/big-service/files/big-service.py
@@ -3,8 +3,23 @@
 import BaseHTTPServer
 
 SERVER_ADDRESS = ('0.0.0.0', 7777)
+get_request_counter = 0
+
 
 class BigHTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    """
+    implements handler for cools website that count your access to it :)
+    """
+    PAGE_TEMPLATE = """
+    <html>
+        <head>
+            <title>BigService cool page</title>
+        </head>
+        <body>
+            <h1>AHOY! you are visitor number {visitor_number}!</h1>
+        </body>
+    </html>
+    """
 
     def do_HEAD(self):
         self.send_response(200)
@@ -12,21 +27,27 @@ class BigHTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        if not 'get_request_counter' in globals():
-            global get_request_counter
-            get_request_counter = 0
-        get_request_counter += 1
         self.send_response(200)
         self.send_header("Content-type", 'text/html')
         self.end_headers()
-        self.wfile.write("<html><head><title>BigService cool page</title></head>")
-        self.wfile.write("<body>")
-        self.wfile.write("<h1>AHOY! you are visitor number {0}!</h1>".format(get_request_counter))
-        self.wfile.write("</body></html>")
-        
+        visitor_number = self._get_visitor_number()
+        self.wfile.write(self.PAGE_TEMPLATE.format(visitor_number=visitor_number))
+
+    @staticmethod
+    def _get_visitor_number(self):
+        """
+        increase the get requests counter by one and return its value
+        """
+        if 'get_request_counter' not in globals():
+            global get_request_counter
+            get_request_counter = 0
+        get_request_counter += 1
+        return get_request_counter
+
+
 def main():
     http_server = BaseHTTPServer.HTTPServer(SERVER_ADDRESS, BigHTTPHandler)
-    print 'Server start'
+    print 'Server start on address ' + str(SERVER_ADDRESS)
     http_server.serve_forever()
 
 if __name__ == '__main__':
